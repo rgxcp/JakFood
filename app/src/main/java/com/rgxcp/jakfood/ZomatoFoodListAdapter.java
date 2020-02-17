f81d5bc9c5fb2fd4786df9b65c12ded57cd36309
Rommy Gustiawan <rgcppp@gmail.com>
2020-02-17 11:31:47 +0700

    Implement zomato api to adapter

--- app/src/main/java/com/rgxcp/jakfood/ZomatoFoodListAdapter.java
+++ app/src/main/java/com/rgxcp/jakfood/ZomatoFoodListAdapter.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,60 +14,153 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ZomatoFoodListAdapter extends RecyclerView.Adapter<ZomatoFoodListAdapter.ZomatoFoodListViewHolder> {
+public class ZomatoFoodListAdapter extends RecyclerView.Adapter<ZomatoFoodListAdapter.ListViewHolder> implements Filterable {
 
+    // Setup constructor
     private Context mContext;
-    private List<ZomatoFoodList> mZomatoFoodList;
+    private List<ZomatoFoodList> mList;
+    private List<ZomatoFoodList> mListFilter;
 
-    ZomatoFoodListAdapter(Context mContext, List<ZomatoFoodList> mZomatoFoodList) {
+    // Constructor
+    ZomatoFoodListAdapter(Context mContext, List<ZomatoFoodList> mList) {
         this.mContext = mContext;
-        this.mZomatoFoodList = mZomatoFoodList;
+        this.mList = mList;
+        mListFilter = new ArrayList<>(mList);
     }
 
+    // Recycler view methods
     @NonNull
     @Override
-    public ZomatoFoodListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ZomatoFoodListViewHolder(LayoutInflater.from(mContext).inflate(R.layout.recyclerview_food_list, parent, false));
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ListViewHolder(LayoutInflater.from(mContext).inflate(R.layout.recyclerview_food_list, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ZomatoFoodListViewHolder holder, int position) {
-        final String mRestaurantID = String.valueOf(mZomatoFoodList.get(position).getId());
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+        // Deklarasi dan assign variable lokal || Menangkap data dari Getter
+        Double mStarDouble = Double.valueOf(mList.get(position).getAggregate_rating());
+        String mName = mList.get(position).getName();
+        String mShortAddress = mList.get(position).getLocality_verbose();
+        String mStarString = mList.get(position).getAggregate_rating();
+        String mThumbnail = mList.get(position).getThumb();
+        RequestOptions mRequestOptions = new RequestOptions().centerCrop().placeholder(R.drawable.vc_placeholder).error(R.drawable.vc_placeholder);
 
-        holder.mName.setText(mZomatoFoodList.get(position).getName());
-        holder.mShortAddress.setText(mZomatoFoodList.get(position).getLocality_verbose());
-        holder.mStar.setText(mZomatoFoodList.get(position).getAggregate_rating());
-        Glide.with(mContext).load(mZomatoFoodList.get(position).getThumb()).into(holder.mThumbnail);
+        final String mRestaurantId = mList.get(position).getId();
 
+        // Validasi bintang
+        if (mStarDouble >= 0 && mStarDouble <= 1.9) {
+            holder.mImageStar.setImageResource(R.drawable.ic_star_1);
+        } else if (mStarDouble >= 2 && mStarDouble <= 2.9) {
+            holder.mImageStar.setImageResource(R.drawable.ic_star_2);
+        } else if (mStarDouble >= 3 && mStarDouble <= 3.9) {
+            holder.mImageStar.setImageResource(R.drawable.ic_star_3);
+        } else if (mStarDouble >= 4 && mStarDouble <= 4.9) {
+            holder.mImageStar.setImageResource(R.drawable.ic_star_4);
+        } else {
+            holder.mImageStar.setImageResource(R.drawable.ic_star_5);
+        }
+
+        // Mengisi data layout recycler view
+        holder.mTextName.setText(mName);
+        holder.mTextShortAddress.setText(mShortAddress);
+        holder.mTextStar.setText(mStarString);
+        Glide.with(mContext).load(mThumbnail).apply(mRequestOptions).into(holder.mImageThumbnail);
+
+        // Activities
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent mGotoZomatoFoodDetail = new Intent(mContext, ZomatoFoodDetailActivity.class);
-                mGotoZomatoFoodDetail.putExtra("IDArgs", mRestaurantID);
+                mGotoZomatoFoodDetail.putExtra("RestaurantIdArgs", mRestaurantId);
                 mContext.startActivity(mGotoZomatoFoodDetail);
             }
         });
     }
 
     @Override
-    public int getItemCount() {
-        return mZomatoFoodList.size();
+    public long getItemId(int position) {
+        return position;
     }
 
-    class ZomatoFoodListViewHolder extends RecyclerView.ViewHolder {
-        ImageView mThumbnail;
-        TextView mName, mShortAddress, mStar;
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
 
-        ZomatoFoodListViewHolder(@NonNull View itemView) {
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    // Search view methods
+    @Override
+    public Filter getFilter() {
+        return mFilter;
+    }
+
+    private Filter mFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            // Setup list
+            List<ZomatoFoodList> mListFiltered = new ArrayList<>();
+
+            // Cari list
+            if (charSequence == null || charSequence.length() == 0) {
+                mListFiltered.addAll(mListFilter);
+            } else {
+                String mQuery = charSequence.toString().toLowerCase().trim();
+                for (ZomatoFoodList mZomatoFoodList : mListFilter) {
+                    // Berdasarkan nama restoran
+                    if (mZomatoFoodList.getName().toLowerCase().contains(mQuery)) {
+                        mListFiltered.add(mZomatoFoodList);
+                    }
+
+                    // Berdasarkan alamat restoran
+                    if (mZomatoFoodList.getLocality_verbose().toLowerCase().contains(mQuery)) {
+                        mListFiltered.add(mZomatoFoodList);
+                    }
+                }
+            }
+
+            // Setup hasil
+            FilterResults mFilterResults = new FilterResults();
+            mFilterResults.values = mListFiltered;
+
+            // Menampilkan hasil
+            return mFilterResults;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            mList.clear();
+            mList.addAll((ArrayList) filterResults.values);
+            notifyDataSetChanged();
+        }
+    };
+
+    // Class
+    class ListViewHolder extends RecyclerView.ViewHolder {
+
+        // Deklarasi variable global
+        ImageView mImageStar, mImageThumbnail;
+        TextView mTextName, mTextShortAddress, mTextStar;
+
+        // Constructor
+        ListViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            mThumbnail = itemView.findViewById(R.id.img_rfl_restaurant_thumbnail);
-            mName = itemView.findViewById(R.id.txt_rfl_restaurant_name);
-            mShortAddress = itemView.findViewById(R.id.txt_rfl_short_address);
-            mStar = itemView.findViewById(R.id.txt_rfl_star);
+            // Assign variable global
+            mImageStar = itemView.findViewById(R.id.img_rfl_star);
+            mImageThumbnail = itemView.findViewById(R.id.img_rfl_restaurant_thumbnail);
+            mTextName = itemView.findViewById(R.id.txt_rfl_restaurant_name);
+            mTextShortAddress = itemView.findViewById(R.id.txt_rfl_short_address);
+            mTextStar = itemView.findViewById(R.id.txt_rfl_star);
         }
     }
 }
