4d33bf58bfff0a0dc61757080cecb91aefcf520e
Rommy Gustiawan <rgcppp@gmail.com>
2020-02-20 12:44:28 +0700

    Update zomato photo & review model

--- app/src/main/java/com/rgxcp/jakfood/ZomatoPhoto.java
+++ app/src/main/java/com/rgxcp/jakfood/ZomatoPhoto.java
@@ -1,4 +1,31 @@
 package com.rgxcp.jakfood;
 
 public class ZomatoPhoto {
+
+    private String thumb_url, url;
+
+    ZomatoPhoto() {
+        //
+    }
+
+    public ZomatoPhoto(String thumb_url, String url) {
+        this.thumb_url = thumb_url;
+        this.url = url;
+    }
+
+    String getThumb_url() {
+        return thumb_url;
+    }
+
+    String getUrl() {
+        return url;
+    }
+
+    void setThumb_url(String thumb_url) {
+        this.thumb_url = thumb_url;
+    }
+
+    void setUrl(String url) {
+        this.url = url;
+    }
 }
--- app/src/main/java/com/rgxcp/jakfood/ZomatoReview.java
+++ app/src/main/java/com/rgxcp/jakfood/ZomatoReview.java
@@ -1,4 +1,59 @@
 package com.rgxcp.jakfood;
 
 public class ZomatoReview {
+
+    private Integer rating;
+    private String name, profile_image, review_text, review_time_friendly;
+
+    ZomatoReview() {
+        //
+    }
+
+    public ZomatoReview(Integer rating, String name, String profile_image, String review_text, String review_time_friendly) {
+        this.rating = rating;
+        this.name = name;
+        this.profile_image = profile_image;
+        this.review_text = review_text;
+        this.review_time_friendly = review_time_friendly;
+    }
+
+    Integer getRating() {
+        return rating;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    String getProfile_image() {
+        return profile_image;
+    }
+
+    String getReview_text() {
+        return review_text;
+    }
+
+    String getReview_time_friendly() {
+        return review_time_friendly;
+    }
+
+    void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setProfile_image(String profile_image) {
+        this.profile_image = profile_image;
+    }
+
+    void setReview_text(String review_text) {
+        this.review_text = review_text;
+    }
+
+    void setReview_time_friendly(String review_time_friendly) {
+        this.review_time_friendly = review_time_friendly;
+    }
 }
