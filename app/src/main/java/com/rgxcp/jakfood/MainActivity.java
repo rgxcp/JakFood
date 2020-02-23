67554c8b75fb396a071c28bab844078cf651f9ea
Rommy Gustiawan <rgcppp@gmail.com>
2020-02-23 17:05:04 +0700

    Add fragment transition in main activity

--- app/src/main/java/com/rgxcp/jakfood/MainActivity.java
+++ app/src/main/java/com/rgxcp/jakfood/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -60,7 +61,10 @@ public class MainActivity extends AppCompatActivity {
 
             if (mSelectedFragment != null) {
                 FragmentManager mFragmentManager = getSupportFragmentManager();
-                mFragmentManager.beginTransaction().replace(R.id.fragment_container, mSelectedFragment).commit();
+                mFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, mSelectedFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit();
             }
 
             return true;
